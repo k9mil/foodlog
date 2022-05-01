039fe4fd284c6106562cfadb9c27230eefd1f23e
Kamil Zak <kamilzak00@gmail.com>
2022-05-01 10:29:12 +0100

    feat: added a category relationship

--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -1,7 +1,41 @@
-require "test_helper"
+require 'test_helper'
 
 class EntryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "is valid with valid attributes" do
+    entry = Entry.new(meal_type: "Breakslow", carbohydrates: 19, fats: 18, proteins: 17, calories: 10000)
+
+    assert entry.save
+  end
+  
+  test "Should not save without meal_type" do
+    entry = Entry.new(carbohydrates: 19, fats: 18, proteins: 17, calories: 10000)
+
+    assert_not entry.save
+  end
+  
+    
+  test "Should not save without carbohydrates" do
+    entry = Entry.new(meal_type: "Breakslow", fats: 18, proteins: 17, calories: 10000)
+
+    assert_not entry.save
+  end
+
+    
+  test "Should not save without fats" do
+    entry = Entry.new(meal_type: "Breakslow", carbohydrates: 19, proteins: 17, calories: 10000)
+
+    assert_not entry.save
+  end
+    
+  test "Should not save without proteins" do
+    entry = Entry.new(meal_type: "Breakslow", carbohydrates: 19, fats: 18, calories: 10000)
+
+    assert_not entry.save
+  end
+      
+  test "Should not save without calories" do
+    entry = Entry.new(meal_type: "Breakslow", carbohydrates: 19, fats: 18, proteins: 17)
+
+    assert_not entry.save
+  end
 end
